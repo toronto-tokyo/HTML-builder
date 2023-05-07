8865a7dcbbe465d6865030f0f4b0f45149c8a7b5
Minchuk Ilya <cacatubutyt@gmail.com>
2023-05-07 14:31:43 +0200

    feat: solve 03-files-in-folder task

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const fs = require('fs');
+
+fs.readdir(path.join(__dirname, '/', 'secret-folder'), { withFileTypes: true }, (err, data) => {
+  if (err) {
+    return console.log(err);
+  }
+  data.forEach(el => {
+    if (el.isFile()) {
+      const elPath = path.join(__dirname, '/secret-folder', el.name)
+      const extname = path.extname(elPath);
+      const name = el.name.split(extname).join('');
+      fs.stat(elPath, (err, stats) => {
+        if (err) {
+          return console.log(err);
+        }
+        console.log(`${name}-${extname.slice(1)}-${stats.size}b`)
+      })
+    }
+  })
+});
+
