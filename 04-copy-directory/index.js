4df19ccc77661f50b08b1c21f11d39e2e76d3b59
Minchuk Ilya <cacatubutyt@gmail.com>
2023-05-07 20:25:42 +0200

    feat: solve 04-copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,50 @@
+const fs = require('fs');
+const path = require('path');
+
+async function copyDir(pathFolderFrom, pathFolderInto) {
+  const pathFolderFromStr = pathFolderFrom;
+  const pathFolderIntoStr = pathFolderInto;
+  await fs.promises.mkdir(pathFolderIntoStr, { recursive: true }, (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+  const filesInCopyFolder = await fs.promises.readdir(pathFolderIntoStr, { withFileTypes: true }, (err, data) => {
+    if (err) {
+      return console.log(err);
+    }
+    return data;
+  });
+  if (filesInCopyFolder.length > 0) {
+    filesInCopyFolder.forEach(el => {
+      fs.unlink(path.join(pathFolderIntoStr, el.name), (err) => {
+        if (err) console.log(err);
+      });
+    });
+  }
+  async function copyFiles() {
+    const files = await fs.promises.readdir(pathFolderFromStr, { withFileTypes: true }, (err, data) => {
+      if (err) {
+        return console.log(err);
+      }
+      return data;
+    });
+    if (files.length > 0) {
+      files.forEach(el => {
+        if (el.isFile()) {
+          fs.copyFile(path.join(pathFolderFromStr, el.name), path.join(pathFolderIntoStr, el.name), (err) => {
+            if (err) {
+              return console.log(err);
+            }
+          });
+        } else {
+          copyDir(path.join(pathFolderFromStr, el.name), path.join(pathFolderIntoStr, el.name));
+        }
+      });
+    }
+  }
+  copyFiles();
+}
+
+copyDir(path.join(__dirname, '/', 'files'), path.join(__dirname, '/', 'files-copy'));
 
